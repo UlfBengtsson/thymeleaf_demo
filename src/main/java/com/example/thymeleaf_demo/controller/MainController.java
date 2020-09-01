991c4f8fbe49d89555b4c91b08eded074d345329
Ulf Bengtsson <ulf.bengtsson@lexicon.se>
2020-09-01 11:14:25 +0200

    Initial commit

---
+++ src/main/java/com/example/thymeleaf_demo/controller/MainController.java
@@ -0,0 +1,53 @@
+package com.example.thymeleaf_demo.controller;
+
+import javax.validation.Valid;
+
+import com.example.thymeleaf_demo.dto.DtoCar;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.ObjectError;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+@Controller
+public class MainController {
+
+    private static Collection<DtoCar> carList = new ArrayList<>();
+
+    @GetMapping("/")
+    public String index(Model model) {
+        model.addAttribute("carList", carList);
+        return "index";
+    }
+
+    @GetMapping("/car")
+    public String createCar(Model model) {
+        model.addAttribute("car", new DtoCar());
+        return "carForm";
+    }
+
+    @PostMapping("/car")
+    public String createCar(@Valid @ModelAttribute("car") DtoCar car, BindingResult bindingResult) {
+
+        bindingResult.addError(new FieldError("car", "brand", "Required and no blank spaces"));
+
+        System.out.println("bindingResult has Errors: " + bindingResult.hasErrors());
+
+        if (bindingResult.hasErrors()) {
+            return "carForm";
+        }
+        else {
+            carList.add(car);
+        }
+
+        return "redirect:/";
+    }
+}
---
+++ src/main/java/com/example/thymeleaf_demo/dto/DtoCar.java
@@ -0,0 +1,68 @@
+package com.example.thymeleaf_demo.dto;
+
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+public class DtoCar {
+
+    private int id;
+
+    @NotNull
+    @Size(min=2, max=30)
+    private String brand;
+
+    private String modelName;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getModelName() {
+        return modelName;
+    }
+
+    public void setModelName(String modelName) {
+        this.modelName = modelName;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        DtoCar dtoCar = (DtoCar) o;
+
+        if (getId() != dtoCar.getId()) return false;
+        if (getBrand() != null ? !getBrand().equals(dtoCar.getBrand()) : dtoCar.getBrand() != null) return false;
+        return getModelName() != null ? getModelName().equals(dtoCar.getModelName()) : dtoCar.getModelName() == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = getId();
+        result = 31 * result + (getBrand() != null ? getBrand().hashCode() : 0);
+        result = 31 * result + (getModelName() != null ? getModelName().hashCode() : 0);
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "DtoCar{" +
+                "id=" + id +
+                ", brand='" + brand + '\'' +
+                ", modelName='" + modelName + '\'' +
+                '}';
+    }
+}
